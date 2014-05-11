f4217e8fb0728aad0dd4e51ab8981e66a7dee294
wzaghal <wisamzaghal@gmail.com>
2014-05-11 15:33:01 -0400

    inject title to html with reset behaviour

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,76 @@
 $(document).ready(function(){
 	var movie={};
+	var config;
+	var addWord = function(title){
 
-var movieApp = {
-	api_key: 'ed68b87c3d69c256a7f70b7e9cb008e2',
-	init: function(pageNumber, titleIndex){
+		//logic to change word here
+		for (var i = title.length; i > 0; i -- ){
 
-	$.ajax({
-	  url: 'http://api.themoviedb.org/3/movie/popular',
-	  type: 'GET',
-	  data: {api_key: movieApp.api_key, page: pageNumber},
-	  dataType: 'jsonp',
-	  success: function(response) {
-	  	movie.title=response.results[titleIndex].original_title;
-	  	movie.imageUrl= response.results[titleIndex].backdrop_path;
-	  	console.log(movie);
-	  }
-	});
-	$.ajax({
-	  url: 'http://api.themoviedb.org/3/configuration',
-	  type: 'GET',
-	  data: {api_key: movieApp.api_key},
-	  dataType: 'jsonp',
-	  success: function(response) {
-	  }
-	});
 
+		}
+		$('h2').append(title);
+	};
+	var movieApp = {
+		api_key: 'ed68b87c3d69c256a7f70b7e9cb008e2',
+		test : undefined,
+		init: function(pageNumber, titleIndex){
+			$.ajax({
+				url: 'http://api.themoviedb.org/3/configuration',
+				type: 'GET',
+		  		data: {api_key: movieApp.api_key},
+		  		dataType: 'jsonp',
+				success: function(response) {
+					console.log(response);
+					config = response;
+					movieApp.test = response;
+				}
+		});
+
+		},
+
+		getMovies: function(pageNumber, titleIndex){
+
+		$.ajax({
+		  url: 'http://api.themoviedb.org/3/movie/popular',
+		  type: 'GET',
+		  data: {api_key: movieApp.api_key, page: pageNumber},
+		  dataType: 'jsonp',
+		  success: function(response) {
+		  	movie.title=response.results[titleIndex].original_title;
+		  	movie.imageUrl= response.results[titleIndex].backdrop_path;
+		  	addWord(movie.title);
+		  	
+		  
+
+		  }
+		});
+		}		
+
+		
+
+	};
+
+	var reset = function(){
+		pageNumber= (Math.floor( Math.random() * 5)+1);
+		titleIndex = (Math.floor( Math.random() * 20)+1);
+
+		$('h2').empty();
+
+		movieApp.getMovies(pageNumber, titleIndex);
 	}
+		var pageNumber= (Math.floor( Math.random() * 5)+1);
+		var titleIndex = (Math.floor( Math.random() * 20)+1);
 
+		movieApp.init();
+		movieApp.getMovies(pageNumber, titleIndex);
+		console.log("movieApp: " + movieApp);
 
-};
+		$('#button').on('click',function(){
+			reset();
 
-var reset = function(){
-	pageNumber= (Math.floor( Math.random() * 5)+1);
-	titleIndex = (Math.floor( Math.random() * 20)+1);
+		});
 
-	movieApp.init(pageNumber, titleIndex);
-}
-	var pageNumber= (Math.floor( Math.random() * 5)+1);
-	var titleIndex = (Math.floor( Math.random() * 20)+1);
-
-	movieApp.init(pageNumber, titleIndex);
-	console.log(movieApp);
-
-	reset();
+		//reset();
 
 
 });
